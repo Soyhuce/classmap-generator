3eb08e9bbab073711c6bfa13c79c2e484ebfe75f
Bastien Philippe <bastien.philippe@soyhuce.fr>
2022-03-02 16:36:57 +0100

    Add more tests

--- tests/ClassmapGeneratorTest.php
+++ tests/ClassmapGeneratorTest.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 use Composer\Autoload\ClassMapGenerator as ComposerClassMapGenerator;
 use Soyhuce\ClassmapGenerator\ClassmapGenerator;
 
-it('Generates same classmap than Composer', function (): void {
-    expect(ClassmapGenerator::createMap(__DIR__ . '/../src'))
-        ->toBe(ComposerClassMapGenerator::createMap(__DIR__ . '/../src'));
-});
+it('Generates same classmap than Composer', function (string $directory): void {
+    expect(ClassmapGenerator::createMap($directory))
+        ->toBe(ComposerClassMapGenerator::createMap($directory));
+})->with([
+    __DIR__ . '/../src',
+    __DIR__ . '/../src/*',
+    __DIR__ . '/../vendor'
+]);
+
